fee02e5e3f46868969bf469163ca268272f586db
ordonez <ordonez@zmt.swiss>
2025-03-20 16:39:34 +0100

    basic call example

---
+++ examples/basic_api_call.py
@@ -0,0 +1,13 @@
+if __name__ == "__main__":
+    from openapi_client import Configuration, ApiClient
+    from openapi_client.api.function_api import FunctionApi
+
+    configuration = Configuration()
+    configuration.host = "http://localhost:8000"
+
+    api_client = ApiClient(configuration=configuration)
+    funcapi = FunctionApi(api_client)
+    print(funcapi.list_functions())
+    """Result (after running "make test"; otherwise probably empty list):
+    [Function(id=1, name='test_function1', type='local.python', url='./examples/test_script/test_function1.py:test_function1', description='Test function 1', input_schema=None, output_schema=None, tags=None), Function(id=2, name='test_function1_slow', type='local.python', url='./examples/test_script/test_function1_slow.py:test_function1', description='Test function 1 (slow)', input_schema={'type': 'object', 'properties': {'x': {'type': 'number'}, 'y': {'type': 'number'}}, 'required': ['x', 'y']}, output_schema={'type': 'object', 'properties': {'result': {'type': 'number'}}, 'required': ['result']}, tags=['cacheable']), Function(id=3, name='test_study1', type='remote.http', url='https://api.osparc.io/studies/2332423423', description='Test study', input_schema=None, output_schema=None, tags=None)]
+    """
